593e673748bd2b4a88794f1fa534ade7d5685e9d
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-07-27 08:19:32 +0000

    Feat: basic Flask app

---
+++ flask_app.py
@@ -0,0 +1,26 @@
+#!python3
+import os
+import json
+from flask import Flask
+from flask_sslify import SSLify
+from flask_cors import cross_origin
+from flask import render_template
+
+app = Flask(__name__, static_url_path='/static')
+sslify = SSLify(app)
+
+global_context = {}
+
+@app.route('/', methods=['GET'])
+def root():
+    return render_template('root.html', **global_context)
+
+
+@app.route('/health.php', methods=['GET'])
+@cross_origin(origins=['alexandersobyanin.ru'], methods=['GET'])
+def health():
+    return '{"health":1}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
