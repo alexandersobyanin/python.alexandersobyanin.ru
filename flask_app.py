547b49c0d90b2944391f7841aca8977ad9ccc15e
Alexander Sobyanin <alexander.sobyanin@gmail.com>
2019-07-27 14:18:12 +0500

    Feat: automatic deploy from github

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,16 @@
 #!python3
 import os
 import json
+import git
 from flask import Flask
+from flask import request
 from flask import render_template
 from flask_sslify import SSLify
 from flask_cors import cross_origin
+from scripts import github
 
 app = Flask(__name__, static_url_path='/static')
+app.debug = False
 sslify = SSLify(app)
 
 global_context = {}
@@ -21,6 +25,22 @@ def root():
 @cross_origin(origins=['alexandersobyanin.ru'], methods=['GET'])
 def health():
     return '{"health":1}'
+
+
+@app.route('/update_server', methods=['POST'])
+def update_server_webhook():
+    if request.method != 'POST':
+        return 'Wrong event type', 405
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    if not x_hub_signature:
+        return 'Wrong params', 402
+    w_secret = os.getenv('UPDATE_SERVER_WEBHOOK_SECRET_KEY')
+    if not github.is_valid_signature(x_hub_signature, request.data, w_secret):
+        return 'Access denied', 403
+    repo = git.Repo('~/mysite/')
+    origin = repo.remotes.origin
+    origin.pull()
+    return 'Updated successfully', 200
 
 
 if __name__ == '__main__':
