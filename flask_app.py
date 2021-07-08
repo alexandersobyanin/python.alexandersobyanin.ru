87824f440ee9a3b19f3ca2c0795633fbf5a28de8
Alexander Sobyanin <me@alexandersobyanin.ru>
2021-07-09 03:48:53 +0500

    Feat: Telegram Bot Webhooks

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,38 @@
 import os
 import json
 import git
+import urllib3
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask_sslify import SSLify
 from flask_cors import cross_origin
+import telepot
 from scripts import github
+
+
+telegram_secret = os.getenv('TELEGRAM_BOT_SECRET')
+
+telegram_euc_urals_radio_bot_token = os.getenv('TELEGRAM_BOT_EUC_URALS_RADIO_BOT')
+telegram_euc_urals_radio_bot_url = '/telegram/TELEGRAM_BOT_EUC_URALS_RADIO_BOT/{}/'.format(telegram_secret)
+telegram_euc_urals_radio_bot = telepot.Bot(telegram_euc_urals_radio_bot_token)
+telegram_euc_urals_radio_bot.setWebhook(telegram_euc_urals_radio_bot_url, max_connections=1)
+
+telegram_euc_urals_pets_bot_token = os.getenv('TELEGRAM_BOT_EUC_URALS_PETS_BOT')
+telegram_euc_urals_pets_bot_url = '/telegram/TELEGRAM_BOT_EUC_URALS_PETS_BOT/{}/'.format(telegram_secret)
+telegram_euc_urals_pets_bot = telepot.Bot(telegram_euc_urals_pets_bot_token)
+telegram_euc_urals_pets_bot.setWebhook(telegram_euc_urals_pets_bot_url, max_connections=1)
+
+telegram_sret_shot_bot_token = os.getenv('TELEGRAM_BOT_SRET_SHOT_BOT')
+telegram_sret_shot_bot_url = '/telegram/TELEGRAM_BOT_SRET_SHOT_BOT/{}/'.format(telegram_secret)
+telegram_sret_shot_bot = telepot.Bot(telegram_sret_shot_bot_token)
+telegram_sret_shot_bot.setWebhook(telegram_sret_shot_bot_url, max_connections=1)
+
+telegram_must_do_it_bot_token = os.getenv('TELEGRAM_BOT_MUST_DO_IT_BOT')
+telegram_must_do_it_bot_url = '/telegram/TELEGRAM_BOT_MUST_DO_IT_BOT/{}/'.format(telegram_secret)
+telegram_must_do_it_bot = telepot.Bot(telegram_must_do_it_bot_token)
+telegram_must_do_it_bot.setWebhook(telegram_must_do_it_bot_url, max_connections=1)
+
 
 app = Flask(__name__, static_url_path='/static')
 app.debug = False
@@ -25,6 +51,62 @@ def root():
 @cross_origin(origins=['alexandersobyanin.ru'], methods=['GET'])
 def health():
     return '{"health":1}'
+
+
+@app.route(telegram_euc_urals_radio_bot_url, methods=["POST"])
+def telegram_euc_urals_radio_bot_webhook():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        telegram_euc_urals_pets_bot.sendMessage(chat_id, "Как слышно, моноколёсник? Приём!")
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            telegram_euc_urals_radio_bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            telegram_euc_urals_radio_bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
+
+
+@app.route(telegram_euc_urals_pets_bot_url, methods=["POST"])
+def telegram_euc_urals_pets_bot_webhook():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        telegram_euc_urals_pets_bot.sendMessage(chat_id, "Привёт, монокотята!")
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            telegram_euc_urals_pets_bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            telegram_euc_urals_pets_bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
+
+
+@app.route(telegram_sret_shot_bot_url, methods=["POST"])
+def telegram_sret_shot_bot_webhook():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        telegram_sret_shot_bot.sendMessage(chat_id, "Скоро сможете прислать скриншот…")
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            telegram_sret_shot_bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            telegram_sret_shot_bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
+
+
+@app.route(telegram_must_do_it_bot_url, methods=["POST"])
+def telegram_must_do_it_bot_webhook():
+    update = request.get_json()
+    if "message" in update:
+        chat_id = update["message"]["chat"]["id"]
+        telegram_must_do_it_bot.sendMessage(chat_id, "JUST DO IT!")
+        if "text" in update["message"]:
+            text = update["message"]["text"]
+            telegram_must_do_it_bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        else:
+            telegram_must_do_it_bot.sendMessage(chat_id, "From the web: sorry, I didn't understand that kind of message")
+    return "OK"
 
 
 @app.route('/update_server', methods=['POST'])
